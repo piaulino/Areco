52027c532a242f901c796235450e793ad242836c
George <piaulino@gmail.com>
2024-12-11 23:51:47 -0300

    Correção de formato de arquivos
    Alteramos o formato dos arquivos .pas de ANSI para UTF-8 para corrigir um problema onde o GitHub não reconhecia os acentos. Agora, os caracteres especiais são exibidos corretamente no repositório.

---
+++ Global/UMensagens.pas
@@ -0,0 +1,13 @@
+﻿unit UMensagens;
+
+interface
+
+resourcestring
+  ComSucesso  = 'Operação realizada com sucesso.';
+  SemSucesso  = 'Erro ao executar a operação: ';
+  ErroSintaxe = 'Verifique a sintaxe SQL ou inexistência de entidade, atributos no banco de dados ou procesamento do XML';
+
+implementation
+
+end.
+
---
+++ Web Service Teste/__recovery/UWMTeste.pas
@@ -0,0 +1,90 @@
+unit UWMTeste;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Web.HTTPApp, Soap.InvokeRegistry,
+  Soap.WSDLIntf, System.TypInfo, Soap.WebServExp, Soap.WSDLBind, Xml.XMLSchema,
+  Soap.WSDLPub, Soap.SOAPPasInv, Soap.SOAPHTTPPasInv, Soap.SOAPHTTPDisp,
+  Soap.WebBrokerSOAP;
+
+type
+  TWebModule1 = class(TWebModule)
+    HTTPSoapDispatcher1: THTTPSoapDispatcher;
+    HTTPSoapPascalInvoker1: THTTPSoapPascalInvoker;
+    WSDLHTMLPublish1: TWSDLHTMLPublish;
+    procedure WebModule1DefaultHandlerAction(Sender: TObject;
+      Request: TWebRequest; Response: TWebResponse; var Handled: Boolean);
+    procedure WebModuleAfterDispatch(Sender: TObject; Request: TWebRequest;
+      Response: TWebResponse; var Handled: Boolean);
+  private
+    procedure LogRequestDetails(Request: TWebRequest);
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  WebModuleClass: TComponentClass = TWebModule1;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+uses UMensagens;
+
+{$R *.dfm}
+
+procedure TWebModule1.LogRequestDetails(Request: TWebRequest);
+var
+  LogFile: TextFile;
+  Param: string;
+  Params: TStringList;
+begin
+
+{$IFDEF MSWINDOWS}
+  AssignFile(LogFile, 'RequestLog.txt');
+  try
+    if FileExists('RequestLog.txt') then
+      Append(LogFile)
+    else
+      Rewrite(LogFile);
+
+    // Log do IP do cliente
+    Writeln(LogFile, 'IP do Cliente: ' + Request.RemoteAddr);
+
+    // Log da URL solicitada
+    Writeln(LogFile, 'URL Solicitada: ' + Request.URL);
+
+    // Log dos parâmetros da requisição
+    Params := TStringList.Create;
+    try
+      for Param in Request.QueryFields do
+        Params.Values[Param] := Request.QueryFields.Values[Param];
+
+      Writeln(LogFile, 'Parâmetros da Requisição: ' + Params.Text);
+    finally
+      Params.Free;
+    end;
+
+    Writeln(LogFile, '-----------------------------');
+  finally
+    CloseFile(LogFile);
+  end;
+  {$ENDIF}
+end;
+
+procedure TWebModule1.WebModule1DefaultHandlerAction(Sender: TObject;
+  Request: TWebRequest; Response: TWebResponse; var Handled: Boolean);
+begin
+  WSDLHTMLPublish1.ServiceInfo(Sender, Request, Response, Handled);
+
+end;
+
+procedure TWebModule1.WebModuleAfterDispatch(Sender: TObject;
+  Request: TWebRequest; Response: TWebResponse; var Handled: Boolean);
+begin
+  LogRequestDetails(Request);
+end;
+
+end.
